8cc381d6313ee382829f1bee617fb569d7506e5a
pupaluper <90027669+pupaluper@users.noreply.github.com>
2021-09-03 07:50:48 +0000

    Deploying to gh-pages from @ pupaluper/delphi@0b419884e3edb223a2ef5f4aaf1a72554bf3501c 🚀

---
+++ _next/static/j6XtX-YQthLQfRNwdSN3e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-74aacd0cdd093a42cae3.js"],"/_error":["static/chunks/pages/_error-ea939aab753d9e9db3bd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
